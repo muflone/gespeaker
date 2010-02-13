913b315dca83ce6f83e073cce371ed227b1c38ba
Muflone <muflone@vbsimple.net>
2010-02-13 16:23:02 +0000

    Preparing for release 0.8

--- po/i18n-2.sh
+++ po/i18n-2.sh
@@ -4,3 +4,19 @@ msginit --input=gespeaker.pot --output-file=it.po --locale=it_IT
 msginit --input=gespeaker.pot --output-file=fr.po --locale=fr_FR
 msginit --input=gespeaker.pot --output-file=es.po --locale=es_ES
 read
+msginit --input=gespeaker.pot --output-file=ru.po --locale=ru_RU
+msginit --input=gespeaker.pot --output-file=ar.po --locale=ar
+msginit --input=gespeaker.pot --output-file=bg.po --locale=bg
+msginit --input=gespeaker.pot --output-file=cs.po --locale=cs
+msginit --input=gespeaker.pot --output-file=da.po --locale=da
+msginit --input=gespeaker.pot --output-file=de.po --locale=de_DE
+msginit --input=gespeaker.pot --output-file=he.po --locale=he
+msginit --input=gespeaker.pot --output-file=hu.po --locale=hu
+msginit --input=gespeaker.pot --output-file=ja.po --locale=ja
+msginit --input=gespeaker.pot --output-file=nl.po --locale=nl_NL
+msginit --input=gespeaker.pot --output-file=pl.po --locale=pl
+msginit --input=gespeaker.pot --output-file=pt.po --locale=pt_PT
+msginit --input=gespeaker.pot --output-file=sk.po --locale=sk
+msginit --input=gespeaker.pot --output-file=tr.po --locale=tr
+msginit --input=gespeaker.pot --output-file=zh_CN.po --locale=zh_CN
+read
