5afe4e39dc3c67800d2376ef3eb21c33f8f50abf
Muflone <webreg@vbsimple.net>
2014-09-06 19:41:26 +0200

    Bumped version number

--- src/handlepaths.py
+++ src/handlepaths.py
@@ -23,7 +23,7 @@ import sys
 __base_path__ = os.path.dirname(os.path.abspath(__file__))
 APP_NAME = 'gespeaker'
 APP_TITLE = 'Gespeaker'
-APP_VERSION = '0.8.3'
+APP_VERSION = '0.8.4'
 
 PATHS = {
   'locale': [
