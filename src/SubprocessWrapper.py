e7b01c63d7d8d474a01ac21c87b4ae6fed750dd5
Muflone <webreg@vbsimple.net>
2015-07-27 00:23:02 +0200

    Updated copyright notices

--- src/SubprocessWrapper.py
+++ src/SubprocessWrapper.py
@@ -1,20 +1,20 @@
 ##
 #   Project: gespeaker - A GTK frontend for espeak  
-#    Author: Fabio Castelli <muflone@vbsimple.net>
-# Copyright: 2009-2014 Fabio Castelli
+#    Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+# Copyright: 2009-2015 Fabio Castelli
 #   License: GPL-2+
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the Free
 #  Software Foundation; either version 2 of the License, or (at your option)
 #  any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #  more details.
-# 
-# On Debian GNU/Linux systems, the full text of the GNU General Public License
-# can be found in the file /usr/share/common-licenses/GPL-2.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
 import subprocess
--- src/gespeakerUI.py
+++ src/gespeakerUI.py
@@ -1,20 +1,20 @@
 ##
 #   Project: gespeaker - A GTK frontend for espeak  
-#    Author: Fabio Castelli <muflone@vbsimple.net>
-# Copyright: 2009-2014 Fabio Castelli
+#    Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+# Copyright: 2009-2015 Fabio Castelli
 #   License: GPL-2+
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the Free
 #  Software Foundation; either version 2 of the License, or (at your option)
 #  any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #  more details.
-# 
-# On Debian GNU/Linux systems, the full text of the GNU General Public License
-# can be found in the file /usr/share/common-licenses/GPL-2.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
 import gtk
@@ -365,7 +365,7 @@ class gespeakerUI(object):
       name=handlepaths.APP_TITLE,
       version=handlepaths.APP_VERSION,
       comment=_('A GTK frontend for espeak'),
-      copyright='Copyright 2009-2014 Fabio Castelli',
+      copyright='Copyright 2009-2015 Fabio Castelli',
       license=handlepaths.read_text_file('doc', 'copyright'), 
       website='http://www.muflone.com/gespeaker/',
       website_label='http://www.muflone.com/gespeaker/',
