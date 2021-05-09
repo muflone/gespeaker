359dd7dd1a8e7b7bad140180a2d785963f2a303c
Muflone <webreg@muflone.com>
2021-05-09 22:32:51 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 ##
 #     Project: Gespeaker
 # Description: A GTK frontend for espeak
@@ -20,72 +19,144 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.dep_util import newer
-from distutils.log import info
-from glob import glob
+import glob
+import itertools
 import os
-import sys
+import shutil
+import subprocess
+
+from distutils.command.install_scripts import install_scripts
+from distutils.command.install_data import install_data
+from distutils.core import setup, Command
+from distutils.log import info
+
+from gespeaker.constants import (APP_NAME,
+                                 APP_VERSION,
+                                 APP_AUTHOR,
+                                 APP_AUTHOR_EMAIL,
+                                 APP_URL,
+                                 APP_DESCRIPTION,
+                                 DOMAIN_NAME)
 
 
-class InstallData(install_data):
+class Install_Scripts(install_scripts):
     def run(self):
-        self.data_files.extend(self._compile_po_files())
+        install_scripts.run(self)
+        self.rename_python_scripts()
+
+    def rename_python_scripts(self):
+        "Rename main executable python script without .py extension"
+        for script in self.get_outputs():
+            if script.endswith(".py"):
+                info('renaming the python script %s -> %s' % (script,
+                                                              script[:-3]))
+                shutil.move(script, script[:-3])
+
+
+class Install_Data(install_data):
+    def run(self):
+        self.install_icons()
+        cmd_translations = Command_Translations(self.distribution)
+        cmd_translations.initialize_options()
+        cmd_translations.finalize_options()
+        cmd_translations.data_files = self.data_files
+        cmd_translations.run()
         install_data.run(self)
 
-    def _compile_po_files(self):
-        data_files = []
+    def install_icons(self):
+        info('Installing icons...')
+        DIR_ICONS = 'icons'
+        for icon_format in os.listdir(DIR_ICONS):
+            icon_dir = os.path.join(DIR_ICONS, icon_format)
+            self.data_files.append((
+                os.path.join('share', 'icons', 'hicolor', icon_format, 'apps'),
+                glob.glob(os.path.join(icon_dir, '*'))))
 
-        # Don't install language files on win32
-        if sys.platform == 'win32':
-            return data_files
 
-        path_po = 'po'
-        for lang in open(os.path.join(path_po, 'availables'), 'r').readlines():
-            lang = lang.strip()
-            if lang:
-                po = os.path.join(path_po, '%s.po' % lang)
-                mo = os.path.join('build', 'mo', lang, 'gespeaker.mo')
+class Command_Translation(Command):
+    description = "compile a translation"
+    user_options = [
+        ('input=', None, 'Define input file'),
+        ('output=', None, 'Define output file'),
+    ]
 
-                directory = os.path.dirname(mo)
-                if not os.path.exists(directory):
-                    info('creating %s' % directory)
-                    os.makedirs(directory)
+    def initialize_options(self):
+        self.input = None
+        self.output = None
+        self.data_files = []
 
-                if newer(po, mo):
-                    # True if mo doesn't exist
-                    cmd = 'msgfmt -o %s %s' % (mo, po)
-                    info('compiling %s -> %s' % (po, mo))
-                    if os.system(cmd) != 0:
-                        raise SystemExit('Error while running msgfmt')
+    def finalize_options(self):
+        assert (self.input), 'Missing input file'
+        assert (self.output), 'Missing output file'
 
-                dest = os.path.dirname(
-                    os.path.join('share', 'locale', lang, 'LC_MESSAGES',
-                                 'gespeaker.mo'))
-                data_files.append((dest, [mo]))
+    def run(self):
+        """Compile a single translation using self.input and self.output"""
+        lang = os.path.basename(self.input[:-3])
+        dir_lang = os.path.dirname(self.output)
+        if not os.path.exists(dir_lang):
+            os.makedirs(dir_lang)
+        subprocess.call(('msgfmt', '--output-file', self.output, self.input))
+        self.data_files.append((os.path.join('share',
+                                             'locale',
+                                             lang,
+                                             'LC_MESSAGES'),
+                                [self.output]))
 
-        return data_files
+
+class Command_Translations(Command):
+    description = "build translations"
+    user_options = []
+
+    def initialize_options(self):
+        self.data_files = []
+
+    def finalize_options(self):
+        self.dir_base = os.path.dirname(os.path.abspath(__file__))
+        self.dir_po = os.path.join(self.dir_base, 'po')
+        self.dir_mo = os.path.join(self.dir_base, 'locale')
+
+    def run(self):
+        """Compile every translation and add it to the data files"""
+        for file_po in glob.glob(os.path.join(self.dir_po, '*.po')):
+            file_mo = os.path.join(self.dir_mo,
+                                   os.path.basename(file_po[:-3]),
+                                   'LC_MESSAGES',
+                                   '%s.mo' % DOMAIN_NAME)
+            cmd_translation = Command_Translation(self.distribution)
+            cmd_translation.initialize_options()
+            cmd_translation.input = file_po
+            cmd_translation.output = file_mo
+            cmd_translation.finalize_options()
+            # Add the translation files to the data files
+            cmd_translation.data_files = self.data_files
+            cmd_translation.run()
 
 
 setup(
-    name='Gespeaker',
-    version='0.8.5',
-    description='A GTK+ frontend for eSpeak and mbrola',
-    author='Fabio Castelli',
-    author_email='muflone@muflone.com',
-    url='http://www.muflone.com/gespeaker/',
+    name=APP_NAME,
+    version=APP_VERSION,
+    description=APP_DESCRIPTION,
+    author=APP_AUTHOR,
+    author_email=APP_AUTHOR_EMAIL,
+    maintainer=APP_AUTHOR,
+    maintainer_email=APP_AUTHOR_EMAIL,
+    url=APP_URL,
     license='GPL v3',
-    scripts=['gespeaker'],
+    scripts=['gespeaker.py'],
     data_files=[
         ('share/applications', ['data/gespeaker.desktop']),
         ('share/gespeaker/data', ['data/testing.wav']),
-        ('share/gespeaker/data/icons', glob('data/icons/*')),
-        ('share/gespeaker/data/ui', glob('data/ui/*.glade')),
-        ('share/doc/gespeaker',
-         ['doc/README.md', 'doc/changelog', 'doc/translators']),
+        ('share/gespeaker/data/icons', glob.glob('data/icons/*')),
+        ('share/gespeaker/data/ui', glob.glob('ui/*.glade')),
+        ('share/doc/gespeaker', list(itertools.chain(glob.glob('doc/*'),
+                                                     glob.glob('*.md')))),
         ('share/man/man1', ['man/gespeaker.1']),
-        ('share/gespeaker/src', glob('src/*.py'))
+        ('share/gespeaker/src', glob.glob('src/*.py'))
     ],
-    cmdclass={'install_data': InstallData}
+    cmdclass={
+        'install_scripts': Install_Scripts,
+        'install_data': Install_Data,
+        'translation': Command_Translation,
+        'translations': Command_Translations
+    }
 )
